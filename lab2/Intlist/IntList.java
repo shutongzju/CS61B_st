e6649342de985602fbeb2e81c283eebc89969640
shutong <shutongzju@gmail.com>
2020-12-05 23:17:56 +0800

    lab2 done

--- lab2/Intlist/IntList.java
+++ lab2/Intlist/IntList.java
@@ -82,7 +82,15 @@ public class IntList {
 
     public static IntList dcatenate(IntList A, IntList B) {
         //TODO:  fill in method
-        return null;
+        IntList p = A;
+        if(A == null) {
+            return B;
+        }
+        while(A.rest != null) {
+            A = A.rest;
+        }
+        A.rest = B;
+        return p;
     }
 
     /**
@@ -91,7 +99,10 @@ public class IntList {
      */
     public static IntList catenate(IntList A, IntList B) {
         //TODO:  fill in method
-        return null;
+        if(A == null){
+            B;
+        }
+        return new IntList(A.first, catenate(A.rest, B));
     }
 
 
